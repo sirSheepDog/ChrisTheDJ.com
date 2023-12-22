0ae756df9a0bdea569ebd70ef0e6dda674fad9fa
sirSheepDog <tcsswim@gmail.com>
2023-12-21 21:51:05 -0500

    Changed location (again)

---
+++ smoothscroll.1bb14e6c.js
@@ -0,0 +1,496 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"js/smoothscroll.js":[function(require,module,exports) {
+function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+/**
+ * SmoothScroll
+ * This helper script created by DWUser.com.  Copyright 2013 DWUser.com.  
+ * Dual-licensed under the GPL and MIT licenses.  
+ * All individual scripts remain property of their copyrighters.
+ * Date: 10-Sep-2013
+ * Version: 1.0.1
+ */
+
+if (!window['jQuery']) alert('The jQuery library must be included before the smoothscroll.js file.  The plugin will not work propery.');
+
+/**
+ * jQuery.ScrollTo - Easy element scrolling using jQuery.
+ * Copyright (c) 2007-2013 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * @author Ariel Flesler
+ * @version 1.4.3.1
+ */
+;
+(function ($) {
+  var h = $.scrollTo = function (a, b, c) {
+    $(window).scrollTo(a, b, c);
+  };
+  h.defaults = {
+    axis: 'xy',
+    duration: parseFloat($.fn.jquery) >= 1.3 ? 0 : 1,
+    limit: true
+  };
+  h.window = function (a) {
+    return $(window)._scrollable();
+  };
+  $.fn._scrollable = function () {
+    return this.map(function () {
+      var a = this,
+        isWin = !a.nodeName || $.inArray(a.nodeName.toLowerCase(), ['iframe', '#document', 'html', 'body']) != -1;
+      if (!isWin) return a;
+      var b = (a.contentWindow || a).document || a.ownerDocument || a;
+      return /webkit/i.test(navigator.userAgent) || b.compatMode == 'BackCompat' ? b.body : b.documentElement;
+    });
+  };
+  $.fn.scrollTo = function (e, f, g) {
+    if (_typeof(f) == 'object') {
+      g = f;
+      f = 0;
+    }
+    if (typeof g == 'function') g = {
+      onAfter: g
+    };
+    if (e == 'max') e = 9e9;
+    g = $.extend({}, h.defaults, g);
+    f = f || g.duration;
+    g.queue = g.queue && g.axis.length > 1;
+    if (g.queue) f /= 2;
+    g.offset = both(g.offset);
+    g.over = both(g.over);
+    return this._scrollable().each(function () {
+      if (e == null) return;
+      var d = this,
+        $elem = $(d),
+        targ = e,
+        toff,
+        attr = {},
+        win = $elem.is('html,body');
+      switch (_typeof(targ)) {
+        case 'number':
+        case 'string':
+          if (/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)) {
+            targ = both(targ);
+            break;
+          }
+          targ = $(targ, this);
+          if (!targ.length) return;
+        case 'object':
+          if (targ.is || targ.style) toff = (targ = $(targ)).offset();
+      }
+      $.each(g.axis.split(''), function (i, a) {
+        var b = a == 'x' ? 'Left' : 'Top',
+          pos = b.toLowerCase(),
+          key = 'scroll' + b,
+          old = d[key],
+          max = h.max(d, a);
+        if (toff) {
+          attr[key] = toff[pos] + (win ? 0 : old - $elem.offset()[pos]);
+          if (g.margin) {
+            attr[key] -= parseInt(targ.css('margin' + b)) || 0;
+            attr[key] -= parseInt(targ.css('border' + b + 'Width')) || 0;
+          }
+          attr[key] += g.offset[pos] || 0;
+          if (g.over[pos]) attr[key] += targ[a == 'x' ? 'width' : 'height']() * g.over[pos];
+        } else {
+          var c = targ[pos];
+          attr[key] = c.slice && c.slice(-1) == '%' ? parseFloat(c) / 100 * max : c;
+        }
+        if (g.limit && /^\d+$/.test(attr[key])) attr[key] = attr[key] <= 0 ? 0 : Math.min(attr[key], max);
+        if (!i && g.queue) {
+          if (old != attr[key]) animate(g.onAfterFirst);
+          delete attr[key];
+        }
+      });
+      animate(g.onAfter);
+      function animate(a) {
+        $elem.animate(attr, f, g.easing, a && function () {
+          a.call(this, e, g);
+        });
+      }
+    }).end();
+  };
+  h.max = function (a, b) {
+    var c = b == 'x' ? 'Width' : 'Height',
+      scroll = 'scroll' + c;
+    if (!$(a).is('html,body')) return a[scroll] - $(a)[c.toLowerCase()]();
+    var d = 'client' + c,
+      html = a.ownerDocument.documentElement,
+      body = a.ownerDocument.body;
+    return Math.max(html[scroll], body[scroll]) - Math.min(html[d], body[d]);
+  };
+  function both(a) {
+    return _typeof(a) == 'object' ? a : {
+      top: a,
+      left: a
+    };
+  }
+})(jQuery);
+
+/**
+ * jQuery.LocalScroll
+ * Copyright (c) 2007-2010 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * Date: 05/31/2010
+ * @author Ariel Flesler
+ * @version 1.2.8b
+ **/
+;
+(function (b) {
+  function g(a, e, d) {
+    var h = e.hash.slice(1),
+      f = document.getElementById(h) || document.getElementsByName(h)[0];
+    if (f) {
+      a && a.preventDefault();
+      var c = b(d.target);
+      if (!(d.lock && c.is(":animated") || d.onBefore && !1 === d.onBefore(a, f, c))) {
+        d.stop && c._scrollable().stop(!0);
+        if (d.hash) {
+          var a = f.id == h ? "id" : "name",
+            g = b("<a> </a>").attr(a, h).css({
+              position: "absolute",
+              top: b(window).scrollTop(),
+              left: b(window).scrollLeft()
+            });
+          f[a] = "";
+          b("body").prepend(g);
+          location = e.hash;
+          g.remove();
+          f[a] = h;
+        }
+        c.scrollTo(f, d).trigger("notify.serialScroll", [f]);
+      }
+    }
+  }
+  var i = location.href.replace(/#.*/, ""),
+    c = b.localScroll = function (a) {
+      b("body").localScroll(a);
+    };
+  c.defaults = {
+    duration: 1E3,
+    axis: "y",
+    event: "click",
+    stop: !0,
+    target: window,
+    reset: !0
+  };
+  c.hash = function (a) {
+    if (location.hash) {
+      a = b.extend({}, c.defaults, a);
+      a.hash = !1;
+      if (a.reset) {
+        var e = a.duration;
+        delete a.duration;
+        b(a.target).scrollTo(0, a);
+        a.duration = e;
+      }
+      g(0, location, a);
+    }
+  };
+  b.fn.localScroll = function (a) {
+    function e() {
+      return !!this.href && !!this.hash && this.href.replace(this.hash, "") == i && (!a.filter || b(this).is(a.filter));
+    }
+    a = b.extend({}, c.defaults, a);
+    return a.lazy ? this.bind(a.event, function (d) {
+      var c = b([d.target, d.target.parentNode]).filter(e)[0];
+      c && g(d, c, a);
+    }) : this.find("a,area").filter(e).bind(a.event, function (b) {
+      g(b, this, a);
+    }).end().end();
+  };
+})(jQuery);
+
+// Initialize all .smoothScroll links
+jQuery(function ($) {
+  $.localScroll({
+    filter: '.smoothScroll'
+  });
+});
+},{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+var OldModule = module.bundle.Module;
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+  module.bundle.hotData = null;
+}
+module.bundle.Module = Module;
+var checkedAssets, assetsToAccept;
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = "" || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60318" + '/');
+  ws.onmessage = function (event) {
+    checkedAssets = {};
+    assetsToAccept = [];
+    var data = JSON.parse(event.data);
+    if (data.type === 'update') {
+      var handled = false;
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          var didAccept = hmrAcceptCheck(global.parcelRequire, asset.id);
+          if (didAccept) {
+            handled = true;
+          }
+        }
+      });
+
+      // Enable HMR for CSS by default.
+      handled = handled || data.assets.every(function (asset) {
+        return asset.type === 'css' && asset.generated.js;
+      });
+      if (handled) {
+        console.clear();
+        data.assets.forEach(function (asset) {
+          hmrApply(global.parcelRequire, asset);
+        });
+        assetsToAccept.forEach(function (v) {
+          hmrAcceptRun(v[0], v[1]);
+        });
+      } else if (location.reload) {
+        // `location` global exists in a web worker context but lacks `.reload()` function.
+        location.reload();
+      }
+    }
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+      removeErrorOverlay();
+    }
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+      removeErrorOverlay();
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+  return overlay;
+}
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+  var parents = [];
+  var k, d, dep;
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(k);
+      }
+    }
+  }
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+  return parents;
+}
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+function hmrAcceptCheck(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+  if (!modules[id] && bundle.parent) {
+    return hmrAcceptCheck(bundle.parent, id);
+  }
+  if (checkedAssets[id]) {
+    return;
+  }
+  checkedAssets[id] = true;
+  var cached = bundle.cache[id];
+  assetsToAccept.push([bundle, id]);
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    return true;
+  }
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAcceptCheck(global.parcelRequire, id);
+  });
+}
+function hmrAcceptRun(bundle, id) {
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+  delete bundle.cache[id];
+  bundle(id);
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+}
+},{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","js/smoothscroll.js"], null)
+//# sourceMappingURL=/smoothscroll.1bb14e6c.js.map
